462bdae15879d521593a7823dd39126fb23fcafe
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-22 18:33:49 +0500

    Create Marks Sheet

---
+++ 02-Javascript/Class-03/app.js
@@ -0,0 +1,16 @@
+alert ("Welcome");
+
+var TotalMarks = (400)
+
+var UserName = prompt("Enter your name");
+var english = +prompt("Enter English Mark");
+var urdu = +prompt("Enter Urdu Mark");
+var maths = +prompt("Enter Maths Mark");
+var computer = +prompt("Enter Computer Mark");
+
+var ObtainMarks = (english + urdu + urdu + computer)
+var TotalPercentage = (ObtainMarks / TotalMarks * 100)
+
+console.log (TotalPercentage + "%")
+
+
