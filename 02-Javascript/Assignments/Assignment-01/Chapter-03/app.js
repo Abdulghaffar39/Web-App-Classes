61d891b07b96874c8ff200ad37f3cb342bf9048c
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-25 16:57:28 +0500

    Question-2 Completed

--- 02-Javascript/Assignments/Assignment-01/Chapter-03/app.js
+++ 02-Javascript/Assignments/Assignment-01/Chapter-03/app.js
@@ -1,3 +1,25 @@
 var age = 21;
 
-alert ("I am" + age + "year old")
+alert ("I am " + age + " year old")
+
+// -----------------------------------------------------
+
+// Step 1: Check karo ki localStorage mein visitCount hai ya nahi
+var visitCount = localStorage.getItem('visitCount');
+
+// Step 2: Agar visitCount nahi hai, to use 0 set karo
+if (!visitCount) {
+    visitCount = 0;
+}
+
+// Step 3: Visit count ko 1 se increase karo
+visitCount = parseInt(visitCount) + 1;
+
+// Step 4: Updated count ko localStorage mein save karo
+localStorage.setItem('visitCount', visitCount);
+
+// Step 5: Alert box mein visit count show karo
+alert("You have visited this site " + visitCount + " times.");
+
+// -----------------------------------------------------
+
