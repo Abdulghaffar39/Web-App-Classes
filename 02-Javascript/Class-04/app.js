c4598e06634a382e9fe7bed6233350a49797aa4d
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-26 14:22:56 +0500

    chap 4

---
+++ 02-Javascript/Class-04/app.js
@@ -0,0 +1,28 @@
+// Chapter 4 question 1
+
+var num1 = 2,
+  num2 = 7,
+  num3 = 8;
+
+console.log(num1 + num2 + num3);
+
+// Chapter 4 question 2
+
+// legal Name
+
+// var UserName;
+// var $email;
+// var my_password;
+// var $order_1;
+// var g_mail$;
+
+// // Illegal Name
+
+// var var;
+// var 123name;
+// var $username-;
+// var email*123l
+// var @password;
+
+// Chapter 4 question 3
+
