8ce1f992d24ead266234ab8a1b94041df993f831
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-27 18:30:29 +0500

    Chapter 5, Question 1 completed

--- 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
+++ 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
@@ -1,3 +1,54 @@
+// Chapter 5, Question 1 & 2 started
+var num1 = (3);
+var num2 = (5);
+
+var num3 = (20);
+var num4 = (10);
+
+var num5 = (20);
+var num6 = (10);
+
+var num7 = (20);
+var num8 = (10);
+
+var num9 = (20);
+var num10 = (10);
+
+
+
+var total = (num1 + num2);
+var total_sub = (num3 - num4);
+var total_multiply = (num5 * num6);
+var total_Divide = (num7 / num8);
+var total_Modulus = (num9 % num10);
+
+
+
+document.write(
+  "Sum of " + num1 + " & " + num2 + " is " + total +
+
+    " || " +
+
+    "Subtract of " + num3 + " & " + num4 + " is " + total_sub +
+
+    " || " +
+
+    "Multiply of " + num5 + " & " + num6 + " is " + total_multiply +
+
+    " || " +
+
+    "Divide of " + num5 + " & " + num6 + " is " + total_Divide 
+
+    + " || " +
+
+    "Modulus of " + num5 + " & " + num6 + " is " + total_Modulus
+);
+// Chapter 5, Question 1 & 2 ended
+
+
+
+
+
 // var Fahrenheit = prompt("Enter Fahrenheit")
 
 // var formula = ( Fahrenheit * 9 / 5 + 32 )
@@ -8,12 +59,13 @@
 
 
 
-var celsius = prompt("Enter Celsius")
+// var celsius = prompt("Enter Celsius")
 
-var formula = (celsius - 32) * 5 / 9;
-
-
-document.write (celsius + formula + "is equal to" + " Fahrenheit")
+// var formula = (celsius - 32) * 5 / 9;
 
 
+// document.write (celsius + formula + "is equal to" + " Fahrenheit")
 
+
+
+ 
