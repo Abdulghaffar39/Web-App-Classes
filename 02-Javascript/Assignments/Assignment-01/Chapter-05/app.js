5db17b4e212552ef9b59483c664ee12a64016f71
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-28 12:28:08 +0500

    chapter 5 question 5

--- 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
+++ 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
@@ -1,49 +1,101 @@
+
 // Chapter 5, Question 1 & 2 started
-var num1 = (3);
-var num2 = (5);
+// var num1 = (3);
+// var num2 = (5);
 
-var num3 = (20);
-var num4 = (10);
+// var num3 = (20);
+// var num4 = (10);
 
-var num5 = (20);
-var num6 = (10);
+// var num5 = (20);
+// var num6 = (10);
 
-var num7 = (20);
-var num8 = (10);
+// var num7 = (20);
+// var num8 = (10);
 
-var num9 = (20);
-var num10 = (10);
-
-
-
-var total = (num1 + num2);
-var total_sub = (num3 - num4);
-var total_multiply = (num5 * num6);
-var total_Divide = (num7 / num8);
-var total_Modulus = (num9 % num10);
+// var num9 = (20);
+// var num10 = (10);
 
 
 
-document.write(
-  "Sum of " + num1 + " & " + num2 + " is " + total +
+// var total = (num1 + num2);
+// var total_sub = (num3 - num4);
+// var total_multiply = (num5 * num6);
+// var total_Divide = (num7 / num8);
+// var total_Modulus = (num9 % num10);
 
-    " || " +
 
-    "Subtract of " + num3 + " & " + num4 + " is " + total_sub +
 
-    " || " +
+// document.write(
+//   "Sum of " + num1 + " & " + num2 + " is " + total +
 
-    "Multiply of " + num5 + " & " + num6 + " is " + total_multiply +
+//     " || " +
 
-    " || " +
+//     "Subtract of " + num3 + " & " + num4 + " is " + total_sub +
 
-    "Divide of " + num5 + " & " + num6 + " is " + total_Divide 
+//     " || " +
 
-    + " || " +
+//     "Multiply of " + num5 + " & " + num6 + " is " + total_multiply +
 
-    "Modulus of " + num5 + " & " + num6 + " is " + total_Modulus
-);
-// Chapter 5, Question 1 & 2 ended
+//     " || " +
+
+//     "Divide of " + num5 + " & " + num6 + " is " + total_Divide 
+
+//     + " || " +
+
+//     "Modulus of " + num5 + " & " + num6 + " is " + total_Modulus
+// );
+// Chapter 5, Question 1 & 2 endedcvxcv
+
+
+
+// Chapter 5, Question 3 started
+// var declaration_1 = ("Value after variable declaration is undefined")
+// var declaration_2 = ("Initial Value: 5")
+// var declaration_3 = ("Value after increment is: 5")
+// var declaration_4 = ("Value after increment is: 5")
+// var declaration_5 = ("Value after Addition is: 13")
+// var declaration_6 = ("Value after decrement is: 12")
+// var declaration_7 = ("The remainder value is: 0")
+
+// var add = declaration_1 + "\n" + declaration_2 + "\n" + declaration_3 + "\n" + declaration_4 + "\n" + declaration_5 + "\n" + declaration_6 + "\n" + declaration_7;
+
+// document.write (add)
+// Chapter 5, Question 3 ended
+
+
+// Chapter 5, Question 3 started
+// var ticket_cost = (600);
+// var ticket = prompt ("How many tickets do you want to buy?")
+
+// var tickets = (ticket_cost * ticket );
+
+// document.write ("Total cost buy " + ticket + " tickets to a movie is " + tickets + " PKR")
+// Chapter 5, Question 3 ended
+
+
+// Chapter 5, Question 3 started
+
+var Multiply = prompt ("Enter Table Number")
+
+var table_1 = (Multiply * 1) 
+
+document.write (Multiply * "1")
+
+// var table_2 =(Multiply * 2) 
+// var table_3 =(Multiply * 3) 
+// var table_4 =(Multiply * 4) 
+// var table_5 =(Multiply * 5) 
+// var table_6 =(Multiply * 6) 
+// var table_7 =(Multiply * 7) 
+// var table_8 =(Multiply * 8) 
+// var table_9 =(Multiply * 9) 
+// var table_10 =(Multiply * 10) 
+// var table_11 =(Multiply * 11) 
+// var table_12 =(Multiply * 12) 
+
+
+
+// Chapter 5, Question 3 ended
 
 
 
