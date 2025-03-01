a65fcd3e4325d079cc18c9161572ee1b1eef4fc8
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-03-01 13:12:49 +0500

    Chapter 5, All question Completed

--- 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
+++ 02-Javascript/Assignments/Assignment-01/Chapter-05/app.js
@@ -63,60 +63,153 @@
 // Chapter 5, Question 3 ended
 
 
-// Chapter 5, Question 3 started
+// Chapter 5, Question 4 started
 // var ticket_cost = (600);
 // var ticket = prompt ("How many tickets do you want to buy?")
 
 // var tickets = (ticket_cost * ticket );
 
 // document.write ("Total cost buy " + ticket + " tickets to a movie is " + tickets + " PKR")
-// Chapter 5, Question 3 ended
+// Chapter 5, Question 4 ended
 
 
-// Chapter 5, Question 3 started
+// Chapter 5, Question 5 started
+// var multiply = prompt ("Enter Table Number")
 
-var Multiply = prompt ("Enter Table Number")
+// var table_1 = ( multiply * 1);
+// var table_2 = ( multiply * 2);
+// var table_3 = ( multiply * 3);
+// var table_4 = ( multiply * 4);
+// var table_5 = ( multiply * 5);
+// var table_6 = ( multiply * 6);
+// var table_7 = ( multiply * 7);
+// var table_8 = ( multiply * 8);
+// var table_9 = ( multiply * 9);
+// var table_10 = ( multiply * 10);
+// var table_11 = ( multiply * 11);
+// var table_12 = ( multiply * 12);
 
-var table_1 = (Multiply * 1) 
-
-document.write (Multiply * "1")
-
-// var table_2 =(Multiply * 2) 
-// var table_3 =(Multiply * 3) 
-// var table_4 =(Multiply * 4) 
-// var table_5 =(Multiply * 5) 
-// var table_6 =(Multiply * 6) 
-// var table_7 =(Multiply * 7) 
-// var table_8 =(Multiply * 8) 
-// var table_9 =(Multiply * 9) 
-// var table_10 =(Multiply * 10) 
-// var table_11 =(Multiply * 11) 
-// var table_12 =(Multiply * 12) 
-
-
-
-// Chapter 5, Question 3 ended
-
-
-
-
-
-// var Fahrenheit = prompt("Enter Fahrenheit")
-
-// var formula = ( Fahrenheit * 9 / 5 + 32 )
+// document.write(multiply + " X  1 " + "= " + table_1 + "</br>");
+// document.write(multiply + " X  2 " + "= " + table_2 + "</br>");
+// document.write(multiply + " X  3 " + "= " + table_3 + "</br>");
+// document.write(multiply + " X  4 " + "= " + table_4 + "</br>");
+// document.write(multiply + " X  5 " + "= " + table_5 + "</br>");
+// document.write(multiply + " X  6 " + "= " + table_6 + "</br> ");
+// document.write(multiply + " X  7 " + "= " + table_7 + "</br>");
+// document.write(multiply + " X  8 " + "= " + table_8 + "</br>");
+// document.write(multiply + " X  9 " + "= " + table_9 + "</br>");
+// document.write(multiply + " X  10 " + "= " + table_10 + "</br>");
+// document.write(multiply + " X  11 " + "= " + table_11 + "</br>");
+// document.write(multiply + " X  12 " + "= " + table_12 + "</br>");
+// Chapter 5, Question 5 ended
 
 
-// document.write (Fahrenheit + " Celsuius" + "is equal to" + formula + " Fahrenheit")
+// Chapter 5, Question 6 started
+// var fahrenheit = prompt("Enter Fahranheit");
+// var celsius_formula = ((fahrenheit - 32) * 5) / 9;
+
+// var celsius = prompt("Enter Celsius");
+// var fahranheit_formula = (celsius * 9 / 5) + 32;
+
+// document.write (celsius + " Celcius " + " is equal to " + fahranheit_formula + " Fahrenheit "  + "</br>" +  fahrenheit + " Fahrenheit " + " is equal to " + celsius_formula + " Celsuius ");
+// Chapter 5, Question 6 ended
+
+
+// Chapter 5, Question 7 started
+var item_1 = (650);
+var ordered_1 = prompt(
+  "Price of items 1 is 650. Ordered Shipping Charges is 100 " + "\n" + "Ordered Quantity of items?"
+);
+
+
+var item_2 = (100);
+var ordered_2 = prompt(
+  "Price of items 2 is 200. Shipping Charges is 100 " + "\n" + "Ordered Quantity of items?"
+);
+
+var shipping_charges = (100)
 
 
 
+// document.write(
+//   "Price of items 1 is: 650 "
+//    + "</br>" + 
+   
+//    " Quantity of items: " + ordered_1 
+//    + "</br>" + 
+   
+//    " Price of items 2 is: 200 " 
+//    + "</br>" +  
+   
+//    "Quantity of items: "  + ordered_2 
+//    + "</br>" + 
+   
+//    "Shipping Charges:  " + shipping_charges  
+//    + "</br>" +  
+   
+//    "Total cost of First order is: " + (item_1 * ordered_1 + shipping_charges) 
+//    + "</br>" + 
+   
+//    " Total cost of Second order is: " + (item_2 * ordered_2 + shipping_charges) 
+//    + "</br></br>" + 
+   
+//    " Total cost of your order is: " + (item_1 * ordered_1 + shipping_charges + item_2 * ordered_2 + shipping_charges)
+// );
+// Chapter 5, Question 7 ended
 
-// var celsius = prompt("Enter Celsius")
 
-// var formula = (celsius - 32) * 5 / 9;
+// Chapter 5, Question 8 started
+// var total_marks = prompt("Put Total Marks");
+// var mark_obntained = prompt("Put Obtained Marks");
+// var percentage = (mark_obntained / total_marks * 100);
+
+// document.write ( "Total Marks: " + total_marks + "</br>" + "Mark Obntained: " + mark_obntained + "</br>" + "Percentage: " + percentage + "%")
+// Chapter 5, Question 8 ended
 
 
-// document.write (celsius + formula + "is equal to" + " Fahrenheit")
+// Chapter 5, Question 9 started
+// var doller = prompt("How much Doller amount want to convert in PKR")
+// var riyal = prompt("How much Riyal amount want to convert in PKR")
+
+// var total_doller = (doller * 279.44);
+// var total_riyal = (riyal * 74.54);
+
+// document.write ("Total US-Doller: " + doller + "</br>" + " Total Currency in PKR: " + total_doller + "</br></br>" + " Total Saudi-Riyal: " + riyal + "</br>" + " Total Currency in PKR: " + total_riyal + "</br></br></br>" )
+// Chapter 5, Question 9 ended
+
+
+// Chapter 5, Question 10 started
+// document.write ("Add 5 multiply 10 and Divide in 2: " +  5 * 10 / 2);
+// Chapter 5, Question 10 ended
+
+
+// Chapter 5, Question 11 started
+// var current_year = (2025);
+// var brith_year = (1992);
+// var your_age = (current_year - brith_year);
+
+// document.write ("Current Year: " + current_year + "</br>" + "Brith Year: " + brith_year + "</br>" + "Your age is: " + your_age)
+// Chapter 5, Question 11 ended
+
+// Chapter 5, Question 12 started
+// var radius = (20);
+// var ph = (3.142);
+
+// document.write ("Radius of a circle: " + radius + "</br>" +  "The circumference is : " + 2 * 3.142 * radius  + "</br>" + "The area is :" + 3.142 * 20 * 20 + "</br>")
+// Chapter 5, Question 12 ended
+
+
+// Chapter 5, Question 13 started
+// var your_age = (15);
+// var max_estimate = (65);
+// var snack_perday = (3);
+
+// var total = (snack_perday * 365) * (max_estimate - your_age)
+
+// document.write ("Favourite Snack: Chocolate Chip" + "</br>" + "Current age: " + your_age + "</br>" + "Estimated Maximum Age: " + max_estimate + "</br>" + "Amount of snack per day:" + snack_perday + "</br>" + "You will need " + total + " Chocolate chip to last you until the ripe old age of " + max_estimate);
+// Chapter 5, Question 13 ended
+
+
 
 
 
