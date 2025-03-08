bee505778bc74a8cf9db2366b1e6a74e1925d796
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-03-08 17:14:46 +0500

    parameter & argument

---
+++ 02-Javascript/05-Function-and-DOM-intro/app.js
@@ -0,0 +1,73 @@
+// Functions
+
+// function myfunc() {
+
+//     console.log("Hello world")
+//     console.log("Hello world")
+//     console.log("Hello world")
+//     console.log("Hello world")
+//     console.log("Hello world")
+
+// }
+
+// myfunc()
+// myfunc()
+
+
+
+
+// --------------------------------------------------
+
+
+
+
+// function greetuser(){
+//     console.log("Malik tum kamiyab ho")
+
+// }
+
+// greetuser()
+// greetuser()
+// greetuser()
+// greetuser()
+
+
+
+
+
+// --------------------------------------------------
+
+
+// parameter (parameters wo variables hote hain jo function ke andar use kiye jaate hain)
+// argument (   Jab aap function ko call karte hain, to aap arguments pass karte hain, jo parameters ki values set karte hain.)
+
+// undefined (Variable declared hai lekin usme koi value assign nahi hui.)
+// null (Jab aap kisi variable ya object ko khaali (empty) set karna chahte hain)
+
+
+
+// --------------------------------------------------
+
+
+
+
+
+
+// --------------------------------------------------
+
+
+
+
+
+
+// --------------------------------------------------
+
+
+
+
+
+
+// --------------------------------------------------
+
+
+
