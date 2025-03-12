1d96c60a659126d0f6849d3d517174162e15e1c3
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-03-12 07:31:10 +0500

    Dom head

--- 02-Javascript/06-Dom-mainpulation/app.js
+++ 02-Javascript/06-Dom-mainpulation/app.js
@@ -1,8 +1,35 @@
 
+// var head = document.querySelector("#head")
+// console.log(head.innerHTML)
+// head.innerHTML = "Abdul Ghaffar"
 
-var head = document.querySelector("#head")
-console.log(head.innerHTML)
-head.innerHTML = "Abdul Ghaffar"
+
+// // -------------------------------------------------------------
+
+// var p = document.querySelector("#para");
+// p.innerHTML = "ABdul"
 
 
+// -------------------------------------------------------------
+
+var h1 = document.querySelector("#head")
+
+function greetuser() {
+
+if (h1.innerHTML === "Hello DOM") {
+
+  h1.innerHTML = "Chnage thorugh js";
+  console.log (h1.innerHTML)
+
+} else {
+
+  h1.innerHTML = "Hello World";
+  console.log(h1.innerHTML)
+
+}
+
+}
+
+
+// -------------------------------------------------------------
 
