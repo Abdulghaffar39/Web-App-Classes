5e5aaca134a36da30b9fee08466364276bc21ecd
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-03-20 22:26:28 +0500

    split & join

--- 02-Javascript/07-Array/app.js
+++ 02-Javascript/07-Array/app.js
@@ -30,53 +30,98 @@
 // ----------------------------------------------------------
 
 // Push (Array ke End me New value store karta ha)
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
-student.push()
-student.push("Aseel")
-console.log(student)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.push()
+// student.push("Aseel")
+// console.log(student)
 
 
 
-// Unshift  (Array ke Start me New value store karta ha)
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
-student.unshift()
-student.unshift("Hassan")
-console.log(student)
+// // Unshift  (Array ke Start me New value store karta ha)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.unshift()
+// student.unshift("Hassan")
+// console.log(student)
 
 
-// Pop (Array ka End wale method ko delete karta ha)
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
-student.pop()
-console.log(student)
+// // Pop (Array ka End wale method ko delete karta ha)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.pop()
+// console.log(student)
 
 
-// Shift  (Array ka Start wale method ko delete karta ha)
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
-student.shift()
-console.log(student)
+// // Shift  (Array ka Start wale method ko delete karta ha)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.shift()
+// console.log(student)
+
+// // ----------------------------------------------------------
+
+// // Splice (Array me agar center me koi cheez [add/delete] karte ha. lakin phele isaa {starting index} dena parta ha)
+
+// // Delete ki misaal
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
+// student.splice(4 , 2)
+// console.log(student)
+
+// var student = ["AbdulGhaffar", "Azmat", "Hammad", "Gayas", "Farhan", "Owais"];
+// student.splice(3 , 1)
+// console.log(student)
+
+// // Add ki misaal
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
+// student.splice(4 , 0 , "Aseel" , "Hassan")
+// console.log(student)
+
+// var student = ["AbdulGhaffar", "Azmat", "Hammad", "Gayas", "Farhan", "Owais"];
+// student.splice(5 , 0 , "Aseel" , "Hassan")
+// console.log(student)
+
+// ----------------------------------------------------------
 
 // ----------------------------------------------------------
 
 
-// Splice (Array me agar center me koi cheez [add/delete] karte ha. lakin phele isaa {starting index} dena parta ha)
+// Change background Color 
 
-// Delete ki misaal
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
-student.splice(4 , 2)
-console.log(student)
+// var body = document.querySelector("body")
 
-var student = ["AbdulGhaffar", "Azmat", "Hammad", "Gayas", "Farhan", "Owais"];
-student.splice(3 , 1)
-console.log(student)
+// function changColor(color) {
 
-// Add ki misaal
-var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
-student.splice(4 , 0 , "Aseel" , "Hassan")
-console.log(student)
+//   body.style.backgroundColor = color
+  
+// }
 
-var student = ["AbdulGhaffar", "Azmat", "Hammad", "Gayas", "Farhan", "Owais"];
-student.splice(5 , 0 , "Aseel" , "Hassan")
-console.log(student)
+// ----------------------------------------------------------
+
+
+// // Join (Method of String) (Ek String ko Array me convert karna ka liya Split method ka istamal kia jata ha)
+
+
+// var username = "Meher Abdul Ghaffar"
+// console.log(username.split("")) 
+// console.log(username.split(" ")) 
+// console.log(username.split("  ")) 
+
+// ----------------------------------------------------------
+
+
+// // Join (Method of String) (Ek Array ko String me convert karna ka liya join method ka istamal kia jata ha)
+
+// var FullName =["Meher" , "Abdul" , "Ghaffar"];
+// // console.log(FullName.join("abdul"))
+// // console.log(FullName.join(" 123 "))
+// // console.log(FullName.join(""))
+// console.log(FullName.join(" "))
+
+// ----------------------------------------------------------
+
+
+// Nested Array
+
+// var arr = [[1 , 2 , 3 , 4 , [5 , 6 , 7 ]]]
+// console.log(arr[0][4])
+
 // ----------------------------------------------------------
 
 
