b4825c49858009ff8923f4f266adbb5d277b6d17
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-05-25 22:15:40 +0500

    Assignment-01

---
+++ 02-Javascript/Assignments/Assignment-01/app.js
@@ -0,0 +1,38 @@
+// 01-need to check weather user insert a big value than the 100 or less . 
+// 02-need to check userfirst value is greater or user second value. 
+// 03-need to check uservalue is perfect divisible by 2 or not 
+// 04-need to check uservalue is perfect divisible by 3 or not  
+// 05-need to check uservalue is perfect divisible by 5 or not 
+// 06-need to check uservalue what is the reminder by divided by 2 
+// 07-need to check uservalue what is the reminder by divided by 3 
+// 08-need to check uservalue what is the reminder by divided by 5
+
+
+
+
+// --------------------------------------------------------------------
+
+// 01-need to check weather user insert a big value than the 100 or less . 
+
+// var value = +prompt('Enter Number')
+
+// if (value === 100){
+//     alert(`The Number is Equal than 100 and Your value is ${value} ` )
+    
+// }else if (value > 100) {
+//     alert(`The Number is Greater than 100 and Your value is ${value} ` )
+    
+// }else if (value < 100) {
+//     alert(`The Number is Smaller than 100 and Your value is ${value} ` )
+    
+// }else{
+//     alert(`Please enter valid value`)
+// }
+
+
+
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
