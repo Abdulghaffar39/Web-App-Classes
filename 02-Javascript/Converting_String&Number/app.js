ef953f6c84795c79a2ea6688cfd77460f2507703
Abdulghaffar <abdulghaffar15a@outlook.com>
2025-07-23 23:57:09 -0700

    Converting method

---
+++ 02-Javascript/Converting_String&Number/app.js
@@ -0,0 +1,124 @@
+// ----------------------------------------------
+
+// Number string me araha ha
+
+// var age = prompt('Enter your age')
+// console.log('Current Age ' + age);
+
+// Problem 
+// Number ki value prompt se string me arahi ha
+
+// ----------------------------------------------
+
+// Converting String to Number (4 Types)
+
+// parseInt
+// Number
+// Plus sign(+)
+// parseFloat
+
+
+// // parseInt
+// var age = parseInt(prompt('Enter Your age!!!'));
+// console.log(`(parseInt) Your age is ${age} and plus 2 in your age ${age +2}`);
+
+
+// // Number
+// var age = Number(prompt('Enter your age!!!'));
+// console.log(`(number) Your age is ${age} and plus 2 in your age ${age +2}`);
+
+
+// // Plus sign(+)
+// var age = +prompt('Enter your age!!!');
+// console.log(`(Plus sign(+)) Your age is ${age} and plus 2 in your age ${age +2}`);
+
+
+// // parseFloat
+// var fraction = parseFloat('1.9000')
+// console.log(fraction);
+
+// ----------------------------------------------
+
+// String number & String number (multipy, divide and minus)
+
+// Multipy
+// Divide
+// Minus
+// But 'plus' ko number me nhi change kare ga 'Concadinate' karde ga
+
+
+// console.log('500' * '10');
+// console.log('500' / '10');
+// console.log('500' - '10');
+// console.log('500' + '10');
+
+
+// Result
+// string me (multiply , divide & minus) dalne se wo number ki tarha kam kare ga
+// lakin (plus) value dalne se wo Concadinate karde ga
+
+// ----------------------------------------------
+
+// String number & Number
+
+// Multipy
+// Divide
+// Minus
+// But 'plus' ko string or number wali value 'Concadinate' karde ga
+
+
+// console.log('500' * 10);
+// console.log('500' / 10);
+// console.log('500' - 10);
+// console.log('500' + 10);
+
+
+// Result
+// ek side string or dousri side number dalne se wo number ki tarha kam kare ga(* , / and -) me
+// lakin (plus) value dalne se wo Concadinate karde ga
+
+// ----------------------------------------------
+
+// String Number & string value
+
+
+// console.log('500 ' - 'Abdul Ghaffar');
+// console.log('500 ' * 'Abdul Ghaffar');
+// console.log('500 ' / 'Abdul Ghaffar');
+// console.log('500 ' + 'Abdul Ghaffar');
+
+
+// Result
+// String Number & String Value ko (* , / and -) ki surat me NaN show kare ga
+// lakin plus ki surat me 'Concadinate' kare ga
+
+// ----------------------------------------------
+
+// Convert string to number and Concadinate
+
+// // parseInt Concadinate 
+// var age = prompt('Enter Your age!!!');
+// console.log(parseInt(age + 1));
+
+// // number Concadinate
+// var age = prompt('Enter Your age!!!');
+// console.log(Number(age + 1));
+
+// // parseFloat Concadinate
+// var age = prompt('Enter Your age!!!');
+// console.log(parseFloat(age + 1));
+
+// Result
+// Ye string ko number me convert karta ha or Concadinate bhi kardeta ha 
+
+// ----------------------------------------------
+
+// // Converting Number to String 
+
+// var number = 2000;
+// console.log(number.toString() + 500);
+
+// Result
+// ye ( ".tostring()" ) number ko string me convert karta hai
+
+// ----------------------------------------------
