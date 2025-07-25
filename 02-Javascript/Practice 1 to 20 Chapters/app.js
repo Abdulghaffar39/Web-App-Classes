ab8c3cd20333ece4e9d3621d3ddeaae81c832908
Abdulghaffar <abdulghaffar15a@outlook.com>
2025-07-24 22:51:54 -0700

    pending

--- 02-Javascript/Practice 1 to 20 Chapters/app.js
+++ 02-Javascript/Practice 1 to 20 Chapters/app.js
@@ -166,10 +166,213 @@
 // var banger = '!';
 // console.log(message + username + banger);
 
-console.log('Thank, ' + 'Azmat ' + 'for Your Visit ' + '!');
+// console.log('Thank, ' + 'Azmat ' + 'for Your Visit ' + '!');
+
+// var username = 'Ali';
+// var message = 'Thanks, ';
+// var banger = '!';
+// var visit = (message + username + banger);
+// console.log(visit)
+
+// var number = '20' + '20';
+// console.log(number);
+
+// ----------------------------------------------------------------
+
+// Prompts
+
+// var a = prompt('Enter anything')
+// var a = window.prompt('Enter window prompt')
+
+// var value = prompt('Enter your value')
+// console.log(value);
+
+// var value = 'what is your ';
+// var genter = 'Genter'
+// var add_gender = prompt(value + genter)
+// console.log(add_gender);
+
+// ----------------------------------------------------------------
+
+// if statements
+
+// Equal (===) Value
+// Check your Data type then give the awnser
+
+// Equal (==) Value
+// Don't check your Data type then give the awnser
+
+// var name = prompt('Enter Name');
+// if(name === 'Abdul Ghaffar'){
+
+//     alert('Correct Awnser')
+// }
+
+// var name = 'Abdul Ghaffar';
+// if(name === 'Abdul Ghaffar'){
+
+//     alert('Correct Awnser')
+// }
+
+// ----------------------------------------------------------------
+
+// Comparison Operators
+
+// var name = 'anas';
+// if( name !== 'ayan'){
+//     alert('Correct')
+// }
+
+// var name = 'ayan';
+// if( name === 'ayan'){
+//     alert('correct')
+// }
+
+// var num1 = 10;
+// var num2 = 102;
+// if(num1 < num2){
+//     alert('correct')
+// }
+
+// var num1 = 10;
+// var num2 = 10;
+// if(num1 <= num2){
+//     alert('correct')
+// }
+
+// var num1 = 15;
+// var num2 = 10;
+// if(num1 => num2){
+//     alert('correct')
+// }
+
+// ----------------------------------------------------------------
+
+// if...else and else if statements
+
+// Long Work
+// var name = 'ali';
+// if(name === 'ali'){
+
+//     alert('correct');
+
+// }
+// if(name !== 'ali'){
+
+//     alert('Wrong');
+
+// }
+
+// // Short Work
+// var name = 'al';
+// if(name === 'ali'){
+
+//     alert('correct');
+
+// }else{ 
+//     alert('Wrong');
+// }
+
+// ----------------------------------------------------------------
+
+// Testing sets of conditions
+
+// var weight = 200;
+// var time = 20;
+
+// if(weight > 150 && time < 25){
+
+//     alert('Correct');
+
+// }else{
+//     alert('Incorrect')
+// }
+
+// var weight = 200;
+// var time = 20;
+// var genter = 'male';
+
+// if(weight > 250 || time < 15 || genter === 'male'){
+
+//     alert('Correct');
+
+// }else{
+//     alert('Incorrect')
+// }
+
+// ----------------------------------------------------------------
+
+// if statements nested
+
+// if (c === d) {
+
+//     if (x === y) {
+//         g = h;
+//     }
+//     else if (a === b) {
+//         g = h;
+//     }
+//     else {
+//         e = f;
+//     }
+
+// }
+// else {
+
+//     e = f;
+    
+// }
+
+// ----------------------------------------------------------------
+
+// Arrays
+
+// var names = ['ayan' , 'anas' , 'akram' , 'amjad' , 'thseen', 'inayat']
+// console.log('Welcome ' + names[5]);
+
+// var mixedArray = [1, "Bob", "Now is", true];
+// console.log(mixedArray[2] , typeof(mixedArray[2]));
+
+// ----------------------------------------------------------------
+
+// Arrays: Adding and removing element; 
+
+// // push     (Array ke End me New value store karta ha)
+// var names = []
+// console.log(names.push('anas' , 'akram' , 'amja d'));
+
+// // pop      (Array ka End wale method ko delete karta ha)
+// var mixedArray = [1, "Bob", "Now is", true];
+// console.log(mixedArray.pop());
+// console.log(mixedArray);
+
+// ----------------------------------------------------------------
+
+// Arrays: Removing, inserting, and extracting elements
+
+// // Unshift  (Array ke Start me New value store karta ha)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.unshift()
+// student.unshift("Hassan")
+// console.log(student)
+
+// // Shift  (Array ka Start wale method ko delete karta ha)
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas"]
+// student.shift()
+// console.log(student)
 
 
+// // Splice (Array me agar center me koi cheez [add/delete] karte ha. lakin phele isaa {starting index} dena parta ha)
 
+// // Delete ki misaal
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
+// student.splice(4 , 2)
+// console.log(student)
+
+// // Add ki misaal
+// var student = ["AbdulGhaffar" , "Azmat" , "Hammad" , "Gayas" , "Farhan" , "Owais"]
+// student.splice(4 , 0 , "Aseel" , "Hassan")
+// console.log(student)
 // ----------------------------------------------------------------
 
 
