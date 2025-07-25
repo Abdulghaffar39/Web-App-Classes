bc0c31a74daafa36a9f0dc245c0cb2f6abcbc8fa
Abdulghaffar <abdulghaffar15a@outlook.com>
2025-07-24 22:06:53 -0700

    concatenating

---
+++ 02-Javascript/Practice 1 to 20 Chapters/app.js
@@ -0,0 +1,177 @@
+// ----------------------------------------------------------------
+
+// Alert
+
+// Correct Way
+// alert('Abdul Ghaffar');
+// window.alert('Some coders write window.alert');
+// alert('Abdul Ghaffar 1 ' + ' Abdul Ghaffar 2');
+// alert('Abdul Ghaffar 1 ' + '\n' + 'Abdul Ghaffar 2');
+// alert(variable call)
+
+// Incorrect Way
+// alert('Abdul Ghaffar 1' , 'Abdul Ghaffar 2');
+// alert('Abdul Ghaffar 1 ' + '</br>' + 'Abdul Ghaffar 2');
+// alert
+// alert''
+
+// ----------------------------------------------------------------
+
+// legal Way OR illegal Way
+
+// // legal Ways 
+// var name = 'ali';
+// var firstName = 'ahemad';
+// var _first_Name_ = 'anas';
+// var $firstName = 'talha';
+// var FirstName = 'huzaifa';
+// var First_Name = 'shahid';
+// var firstName123 = 'shahid';
+
+// // illegal Ways 
+// var 123name = 'ali';
+// var @firstName = 'ahemad';
+// var &firstName = 'anas';
+// var firstName 'talha';
+// var  = 'huzaifa';
+// var 1 = 'hanzala';
+
+
+// ----------------------------------------------------------------
+
+// Variables for Strings
+
+
+// var name = ('Abdul Ghaffar');
+// console.log(name);
+
+// var name = 'Abdul Ghaffar1';
+// console.log(name);
+
+// var name = 'Azmat';
+// name = 'ahmad';
+// console.log(name);
+
+// var fname;
+// var fname = 'Azmat ali';
+// console.log(name);
+
+// var name = 'Akram';
+// var yourFirstName = 'Akram';
+// var a = 'Akram';
+
+// ----------------------------------------------------------------
+
+// Variables for Strings
+
+
+// var weight = 80;
+// console.log(weight);
+
+// var weight = 150;
+// console.log(weight + 50);
+
+// var age = 23;
+// var new_age = age + 3;
+// console.log(new_age);
+
+// var old_age = 50;
+// var gain_age = 10;
+// var age = old_age + gain_age;
+// console.log(age);
+
+
+// var marks = 80;
+// marks = marks + 10;
+// console.log(marks);
+
+
+// ----------------------------------------------------------------
+
+// Math expressions: Familiar Operators
+
+// var num = 2 + 2;
+// var num = 10 - 2;
+// var num = 10 / 2;
+// var num = 10 * 2;
+// var num = 100 % 7;
+// console.log(num);
+
+// var num = 10;
+// var add = num + 23;
+// console.log(add);
+
+// ----------------------------------------------------------------
+
+// Math expressions: Unfamiliar Operators
+
+// Short way & Behind way 
+// num++
+// num = num + 1;
+
+// Increment
+// num++
+
+// Decrement 
+// num--
+
+// var num = 1;
+// var newNum = num++;
+// console.log(newNum);
+
+// var num = 1;
+// var newNum = ++num;
+// console.log(newNum);
+
+// var num = 1;
+// var newNum = num--;
+// console.log(newNum);
+
+// var num = 1;
+// var newNum = --num;
+// console.log(newNum);
+
+
+// ----------------------------------------------------------------
+
+// Math expressions: Eliminating Ambiguity
+
+// var num = 1 + 4 * 5;
+// console.log(num);
+
+// var total_Cost = 1 + (3 * 5);
+// console.log(total_Cost);
+
+// var total_Cost = (3 + 5) * 2;
+// console.log(total_Cost);
+
+// var remainig_value = (3 + 5) * 2 + 3;
+// console.log(remainig_value);
+
+// var math = (3 + 5) * (2 + 3);
+// console.log(math);
+
+// var cost = ((2 * 4) * 4) + 2;
+// console.log(cost);
+
+// ----------------------------------------------------------------
+
+// Concatenating text strings
+
+// var username = 'Abdul Ghaffar';
+// console.log('Thanks, ' + username + '!');
+
+// var username = 'Ali';
+// var message = 'Thanks, ';
+// var banger = '!';
+// console.log(message + username + banger);
+
+console.log('Thank, ' + 'Azmat ' + 'for Your Visit ' + '!');
+
+
+
+// ----------------------------------------------------------------
+
+
+
+
