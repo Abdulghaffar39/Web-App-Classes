d0cc821b571c64b7f301e531ef7894737af7e139
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-08-12 07:12:14 -0700

    pendding

--- 02-Javascript/06-Dom-mainpulation/Class-03/app.js
+++ 02-Javascript/06-Dom-mainpulation/Class-03/app.js
@@ -1,6 +1,34 @@
 
-let checkNodes = document.childNodes[0].childNodes;
-console.log(childNodes);
-document.write(childNodes)
 
-// 
+// var body = document.childNodes;                                     // Show: [<!DOCTYPE html>, html]
+// var body = document.childNodes[0];                                  // Show: <!DOCTYPE html>
+// var body = document.childNodes[1];                                     // Show: <html lang='en'></html>
+// var body = document.childNodes[2];                                     // Show: Undefined
+
+// var body = document.childNodes[0].childNodes;                            // Show: NodeList []
+// var body = document.childNodes[0].childNodes[0];                         // Show: Undefined
+
+// var body = document.childNodes[1].childNodes;                             // Show: [head, text, body]
+// var body = document.childNodes[1].childNodes[0];                         // Show: <head></head>
+// var body = document.childNodes[1].childNodes[1];                         // Show: #text
+// var body = document.childNodes[1].childNodes[2];                         // Show: body
+// var body = document.childNodes[1].childNodes[3];                              // Show: Undedined
+
+// var body = document.childNodes[1].childNodes[0].childNodes;            // Show: [text, meta, text, meta, text, title,] text]
+// var body = document.childNodes[1].childNodes[0].childNodes[0];             // Show: #text
+// var body = document.childNodes[1].childNodes[0].childNodes[1];             // Show: meta
+// var body = document.childNodes[1].childNodes[0].childNodes[2];             // Show: #text
+// var body = document.childNodes[1].childNodes[0].childNodes[3];             // Show: <meta>
+// var body = document.childNodes[1].childNodes[0].childNodes[4];             // Show: #text
+// var body = document.childNodes[1].childNodes[0].childNodes[5];             // Show: <title></title>
+// var body = document.childNodes[1].childNodes[0].childNodes[6];             // Show: #text
+// var body = document.childNodes[1].childNodes[0].childNodes[7];             // Show: Undedined
+
+var body = document.childNodes[2];             // Show: NodeList []
+var body = document.childNodes[1].childNodes[0].childNodes[0].childNodes[0];             // Show: Undedined
+var body = document.childNodes[1].childNodes[0].childNodes[0].childNodes[0];             // Show: Undedined
+
+console.log(body);
+
+
+
