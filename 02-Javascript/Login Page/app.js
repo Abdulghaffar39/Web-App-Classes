a7c453732ce1a29474256d953c5789bda84157a4
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-08-20 23:58:03 -0700

    half created

--- 02-Javascript/Login Page/app.js
+++ 02-Javascript/Login Page/app.js
@@ -109,22 +109,98 @@ function viewBlogs() {
     let home_age = document.getElementById('home_age');
     let home_email = document.getElementById('home_email');
     let home_password = document.getElementById('home_password');
-
-
+    let profile = document.getElementById('profile');
+    
+    profile.innerHTML = user.validUser.name[0];
     home_haed.innerHTML = user.validUser.name;
     home_age.innerHTML = user.validUser.age;
     home_email.innerHTML = user.validUser.email;
     home_password.innerHTML = user.validUser.password;
 }
 
-viewBlogs()
+    
+function Details() {
+  
+    let user = window.localStorage.getItem('currentUser');
+    user = JSON.parse(user);
+    console.log(user);
+    
+    
+    let profile_name = document.getElementById('profile_name');
+    let home_age = document.getElementById('home_age');
+    let home_email = document.getElementById('home_email');
+    let home_password = document.getElementById('home_password');
 
+    let home_age_2 = document.getElementById('home_age_2');
+    let home_email_2 = document.getElementById('home_email_2');
+    let home_password_2 = document.getElementById('home_password_2');
+
+    
+    profile_name.innerHTML = user.validUser.name;
+    home_age.innerHTML = user.validUser.age;
+    home_email.innerHTML = user.validUser.email;
+    home_password.innerHTML = user.validUser.password;
+    
+    home_age_2.innerHTML = user.validUser.age;
+    home_email_2.innerHTML = user.validUser.email;
+    home_password_2.innerHTML = user.validUser.password;
+
+}   
+
+
+
+function sider() {
+
+    let sider = document.getElementById('sider');
+    let main = document.getElementById('main');
+    // let main_2 = document.getElementById('main_2');
+
+    // main_2.style.display = 'none';
+    sider.style.display = 'none';
+    main.style.display = 'flex';
+    main.style.flexDirection = 'column';
+    main.style.justifyContent = 'space-between';
+}
+
+function close_sider() {
+
+    let sider = document.getElementById('sider');
+    let main = document.getElementById('main');
+    // let main_2 = document.getElementById('main_2');
+
+    sider.style.display = 'block';
+    
+    main.style.display = 'none';
+    main.style.flexDirection = 'column';
+    main.style.justifyContent = 'space-between';
+    
+    // main_2.style.display = 'flex';
+    // main_2.style.flexDirection = 'row';
+    // main_2.style.justifyContent = 'space-around';
+}
+
+
+function back_home() {
+
+    window.location.href = 'home.html';
+}
+
+
+function check_profile() {
+
+    window.location.href = 'profile.html';
+}
+
+
+function logout() {
+
+    window.location.href = 'index.html';
+}
 
 function nav_signin() {
 
     window.location.href = 'signup.html';
 }
-
 
 function nav_signup() {
 
