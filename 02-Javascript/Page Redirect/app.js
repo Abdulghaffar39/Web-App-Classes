31fe8d2be431ebcb97c0831f4e41bee0034b9c34
Abdul Ghaffar <abdulghaffar15a@gmail.com>
2025-09-04 00:12:54 -0700

    Page Redirect

--- 02-Javascript/Page Redirect/app.js
+++ 02-Javascript/Page Redirect/app.js
@@ -1,1 +1,18 @@
+function load() {
 
+    setTimeout(() => {
+
+        window.location.href = 'welcome.html';
+        
+    }, 2 * 1000);
+
+}
+
+
+// Ek page se dusra page me jane ke do tarike
+
+// 01: setTimeout 
+// setTimeout(() => {value}, 2 * 1000);
+
+// 02: meta 
+// <meta http-equiv="refresh" content="2 ; url=welcome.html">
