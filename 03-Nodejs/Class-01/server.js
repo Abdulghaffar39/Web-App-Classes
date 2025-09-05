7682d06e6fb543bc8dc43126b8e566eb03ea60f6
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-09-05 03:48:38 -0700

    Server

---
+++ 03-Nodejs/Class-01/server.js
@@ -0,0 +1,28 @@
+// Server : server kabhi machine hai OR kanhi peace of Code hai
+// Example me chata ho ke me dusre PC se bat karlo to ye task me topology ke zarye karwao ga topology ye server ko connect karne ka tareka hai 
+
+// >>>>>>>>>>>>>>>>>> Type of Network Topologies (Machine ki Example) <<<<<<<<<<<<<<<<<<<<
+
+// ===================================== Bus Topology ====================================
+// (Pros: Simple aur cheap. Sabhi computers ek single cable (bus) se connected hote hain)
+
+// ===================================== Star Topology  ====================================
+// (Har computer ek central device (hub/switch/server) se connected hota hai. Communication only central device)
+
+// ===================================== Ring Topology  ====================================
+// (Har computer apne doosre do computers se connect hota hai, aur ek ring (circle) banata hai. Easy to install aur predictable performance)
+
+// ===================================== Mesh Topology  ====================================
+// (Har computer network me har dusre computer se direct connected hota hai. Bohot zyada wiring lagti hai, costly hota hai.)
+
+// ===================================== Tree Topology  ====================================
+// (is me do PC connect hote hai. chote setup ke liya hota hai)
+
+// ==================================== Hybird Topology  ===================================
+// (Do ya zyada topologies ka combination hota hai (jaise Star + Ring, ya Star + Mesh).)
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> Type of Code (Peace of code) <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// Data ko save karwana Server ke zarye
