827c1d6deaa642eb8dfc83dcc92d4c18815d9f10
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-09-13 06:33:20 -0700

    exports

---
+++ 03-Nodejs/Class-01/demo/server.js
@@ -0,0 +1,22 @@
+console.log('Hello World 2');
+
+// const math = require("./math")
+// const {addValue, subValue} = require("./math")
+
+// console.log('Math value is ' +  math.add(2 , 4));
+// console.log('Math value is ' +  math.sub(2 , 4));
+// console.log('Math value is ' +  math.add(2 , 4) + ' and ' + math.sub(10 , 2));
+// console.log('Math value is ' + add(5 ,  6));
+// console.log('Math value is ' + sub(5 ,  9));
+// console.log('Math value is ' + addValue(5 ,  6));
+// console.log('Math value is ' + subValue(5 ,  9));
+// console.log('Math value is ' + math.add(2 , 4));
+// console.log('Math value is ' + math.sub(3 , 5));
+
+
+// File system
+const math = require("fs")
+console.log('Math value is ' , math);
+
+
+
