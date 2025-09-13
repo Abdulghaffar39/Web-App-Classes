7f9a5d143e42270e64bf445c04da6feee01af142
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-09-12 23:01:52 -0700

    change

---
+++ 02-Javascript/Practice 1 to 20 Chapters/14-for loop/app.js
@@ -0,0 +1,17 @@
+// for loops
+
+
+let cleanestCities = ['islamabad' , 'lahore' , 'faislabad' , 'rawalpindi' , 'multan'];
+let cityToCheck = document.getElementById('cityToCheck');
+let para = document.getElementById('para');
+
+
+function check(){
+    
+    if(cityToCheck == cleanestCities[0]){
+    
+        para = cityToCheck;
+        console.log('alsfaslk');
+        
+    };
+}
---
+++ 03-Nodejs/Class-01/Introduction/index.js
@@ -0,0 +1,19 @@
+const abc = async (params) => {
+    try {
+        alert ("helo world ");
+        pro (sadasda)
+     }
+    catch (err) {
+        console.log(err)
+    }
+};
+
+
+const { response } = require('express');
+const http = require('node:http');
+
+http.createServer((req , res) => {
+
+    response.send('hello world')
+
+}).listeners(3000)
