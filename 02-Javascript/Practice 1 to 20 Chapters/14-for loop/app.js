6573f7b69926bfc494d53bb153682e0998d9cbfa
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-09-17 23:07:49 -0700

    update web and app

--- 02-Javascript/Practice 1 to 20 Chapters/14-for loop/app.js
+++ 02-Javascript/Practice 1 to 20 Chapters/14-for loop/app.js
@@ -1,12 +1,12 @@
-// for loops
-let cleanestCities = ['islamabad', 'lahore', 'faislabad', 'rawalpindi', 'multan'];
+// // for loops
+// let cleanestCities = ['islamabad', 'lahore', 'faislabad', 'rawalpindi', 'multan'];
 
-function check() {
+// function check() {
 
-    let cityToCheck = document.getElementById('cityToCheck').value;
-    let para = document.getElementById('para');
+//     let cityToCheck = document.getElementById('cityToCheck').value;
+//     let para = document.getElementById('para');
 
-    // Long Way
+//--------------------------------- Long Way ----------------------------------------
     // ye Bhot lamba kam hai or is me time bhi bhot lage ga
     // if (cityToCheck === cleanestCities[0]) {
 
@@ -40,28 +40,114 @@ function check() {
     // }
 
 
-    // Short Way
+// ---------------------------------- Short Way --------------------------------------
     // Loop sara karm asan kardeta hai km time me
 
-    let isfound = false;
 
-    for (let i = 0; i <= 4; i++) {
+    // for (let i = 0; i <= 4; i++) {
 
-        if (cityToCheck === cleanestCities[i]) {
+    //     if (cityToCheck === cleanestCities[i]) {
 
-            alert(`It's one of the cleanest cities ${cityToCheck}`)
-            isfound = true
-            para.innerHTML = cityToCheck
-            break;
-        }
+    //         alert(`It's one of the cleanest cities ${cityToCheck}`)
+    //         para.innerHTML = cityToCheck
+    //         break;
+    //     }
 
-    }
-
-    if (isfound === false){
-        
-        alert("It's not on the list");
-        return
-    }
-}
+    // }
 
 
+
+// --------------------------------- for loops Flags ---------------------------------------
+
+//     // var matchFound = "no";
+//     var matchFound = false;
+
+//     for (let i = 0; i <= 4; i++) {
+
+//         if (cityToCheck === cleanestCities[i]) {
+
+//             alert(`It's one of the cleanest cities ${cityToCheck}`);
+//             // matchFound = "yes";
+//             matchFound = true;
+//             para.innerHTML = cityToCheck
+//             break;
+//         }
+
+//     }
+
+//     // if (matchFound === "no") {
+//     if (matchFound === false) {
+
+//         alert("Its not of the list!")
+//     }
+
+// }
+
+
+// --------------------------------- for loops Booleans ---------------------------------------
+
+
+//     var matchFound = false;
+
+//     for (let i = 0; i <= 4; i++) {
+
+//         if (cityToCheck === cleanestCities[i]) {
+
+//             alert(`It's one of the cleanest cities ${cityToCheck}`);
+//             matchFound = true;
+//             para.innerHTML = cityToCheck
+//             break;
+//         }
+
+//     }
+
+//     if (matchFound === false) {
+
+//         alert("Its not of the list!")
+//     }
+
+// }
+
+
+// ------------------------------- for loops array length ---------------------------------
+
+// var numElements = cleanestCities.length
+
+// var matchFound = false;
+
+// for (let i = 0; i <= numElements; i++) {
+
+//     console.log(numElements);
+    
+//     if (cityToCheck === cleanestCities[i]) {
+
+//         alert(`It's one of the cleanest cities ${cityToCheck}`);
+//         matchFound = true;
+//         para.innerHTML = cityToCheck
+//         break;
+//     }
+
+// }
+
+// if (matchFound === false) {
+
+//     alert("Its not of the list!")
+// }
+
+    
+//-------------------------- Loop loopus interruptus ----------------------------
+    
+    // for (let i = 0; i < 10; i++) {
+//   console.log("Step:", i);
+
+//   if (i === 3) {
+    //     console.log("Loopus interruptus at i =", i);
+    //     break; // yahan loop ko zabardasti roka ja raha hai
+//   }
+// }
+
+// console.log("Loop khatam ho gayi!");
+
+// Result: loop bas 0, 1, 2, 3 tak chalta hai aur uske baad ruk jaata hai — yehi “loopus interruptus” hai.
+
+// }
