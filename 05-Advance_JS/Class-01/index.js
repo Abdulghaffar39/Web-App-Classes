1163feda2c66b0cb022cc8fd0c32bb6bdf541985
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-11 10:38:45 +0500

    adv js

---
+++ 05-Advance_JS/Class-01/index.js
@@ -0,0 +1,19 @@
+// Advance Javascript
+
+// -------------------------------------------------------------------------------------------
+
+
+// 01- Sets
+
+// Ek JavaScript Set munfarid(unique) values ka majmua hota hai.    
+// Har value sirf aik dafa Set mein mojood ho sakti hai.
+// Ye values kisi bhi qisam ki ho sakti hain — yani primitive(String/Number/Boolean) ya object.
+// primitive :yani aisi value jo object nahi hoti aur directly store hoti hai.
+
+
+const letters = new Set(["a","b","c"]);
+
+console.log(letters.size);
+
+
+// -------------------------------------------------------------------------------------------
