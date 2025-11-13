631d6d94ed25b9bacadd0bca68d9a3c9ad4cf752
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-13 14:34:11 +0500

    2017 completed

---
+++ 05-Advance_JS/Class-04 (2016 to 2020)/app.js
@@ -0,0 +1,64 @@
+// ==========================================================================
+
+// ------------------------- Started 2016 -----------------------------------
+
+// ------------ Exponentiation Operator ---------------
+
+// ** operator pehle number ko doosre number ki taqat par raise karta hai.
+
+// ---------- ** -----------
+
+// let x = 5;
+
+// console.log(x ** 2);
+// console.log(x ** 3);
+
+
+// ---------- Math.pow() -----------
+
+// let x = 5;
+// console.log(Math.pow(x, 2));
+
+
+// x ** y produces the same result as Math.pow(x, y):
+
+// ------------ Exponentiation Assignment ---------------
+
+// The exponentiation assignment operator (**=) raises the value of a variable to the power of the right operand.
+
+// ---------- **= -----------
+
+// let x = 5;
+// console.log(x **= 2);
+
+// -------------------------- Ended 2016 ------------------------------------
+
+// ------------------------- Started 2017 -----------------------------------
+
+// ------------ The padStart() / padEnd() Method ---------------
+
+// padStart() / padEnd() method string ke start/end se extra characters add karta hai taake string ek specified length tak pohonch jaye.
+
+// let text = "5";
+
+// console.log(text.padStart(5,"0"));
+// console.log(text.padEnd(5,"0"));
+
+
+// ------------ getOwnPropertyNames() ---------------
+
+// Object.getOwnPropertyNames() ek array return karta hai jisme object ke saare properties ke naam hote hain, chahe wo loop me dikhen ya na dikhen.
+
+// const person = {
+//   name: "John",
+//   lname: "Dow",
+//   get greeting() {
+//     return "Hello " + this.name + " " + this.lname;
+//   }
+// };
+
+// console.log(Object.getOwnPropertyNames(person));
+
+// -------------------------- Ended 2017 ------------------------------------
+
+// ==========================================================================
