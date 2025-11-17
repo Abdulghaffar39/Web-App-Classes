42154c50489abd9f347dc4df36530ef4c2ef1c4a
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-17 10:04:52 +0500

    react module export types

---
+++ 06-React/Class-01 (Module)/calculator.js
@@ -0,0 +1,22 @@
+let sumData = (a,b) => {
+    return a+b;
+}
+
+let miniData = (a,b) => {
+    return a-b;
+}
+
+
+export { sumData, miniData } // ek se zayada function export kar sakta hai    
+
+// export default sumData 
+//default 1(ek) hi function ko export karne ke liye ata hai
+
+// export karne ka 3(teesra) taika ye hai
+// export let sumData = (a,b) => {
+//     return a+b;
+// }
+
+// export let miniData = (a,b) => {
+//     return a-b;
+// }
---
+++ 06-React/Class-01 (Module)/index.js
@@ -0,0 +1,4 @@
+import {sumData, miniData} from "./calculator.js";
+
+console.log(sumData(10,20));
+console.log(miniData(100,2));
