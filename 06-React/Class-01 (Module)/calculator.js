73a7756b04fb5b2d51f506124b4f641ef1224aef
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-17 10:12:15 +0500

    working

--- 06-React/Class-01 (Module)/calculator.js
+++ 06-React/Class-01 (Module)/calculator.js
@@ -1,22 +1,24 @@
-let sumData = (a,b) => {
-    return a+b;
-}
+// let sumData = (a,b) => {
+//     return a+b;
+// }
 
-let miniData = (a,b) => {
-    return a-b;
-}
+// let miniData = (a,b) => {
+//     return a-b;
+// }
 
 
-export { sumData, miniData } // ek se zayada function export kar sakta hai    
+// export { sumData, miniData } // ek se zayada function export kar sakta hai    
 
 // export default sumData 
 //default 1(ek) hi function ko export karne ke liye ata hai
 
 // export karne ka 3(teesra) taika ye hai
-// export let sumData = (a,b) => {
-//     return a+b;
-// }
+export let sumData = (a,b) => {
+    return a+b;
+}
 
-// export let miniData = (a,b) => {
-//     return a-b;
-// }
+export let miniData = (a,b) => {
+    return a-b;
+};
+
+export const name = "Abdul Ghaffar"
--- 06-React/Class-01 (Module)/index.js
+++ 06-React/Class-01 (Module)/index.js
@@ -1,4 +1,5 @@
-import {sumData, miniData} from "./calculator.js";
+import {sumData, miniData, name} from "./calculator.js";
 
 console.log(sumData(10,20));
 console.log(miniData(100,2));
+console.log(name);
