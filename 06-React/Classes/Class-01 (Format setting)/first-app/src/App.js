1b99dd22b5ae3cbbf731f6c98ac3a979001c7930
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-18 19:31:30 +0500

    working

--- 06-React/Classes/Class-01 (Format setting)/first-app/src/App.js
+++ 06-React/Classes/Class-01 (Format setting)/first-app/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
+import menu from './menu.png';
+import xicon from './x_icon.png';
+import people from './people.webp';
 import './App.css';
+
 
 function App() {
 
@@ -31,8 +35,52 @@ function App() {
 
         </div>
 
-
       </header>
+
+      <main>
+
+        <div className='container_1'>
+
+          <div className='parent_1'>
+
+            <h1>Learn in-demand <span>UX, PM & AI</span> skills</h1>
+            <p>Bite-sized, interactive learning built for busy professionals. Advance your UX design or product management career in 5 minutes a day.</p>
+
+            <div className='child_1'>
+
+              <button className='get_button'>Get Started</button>
+              <button className='teams_button'>Uxcel for Teams</button>
+
+            </div>
+
+            <div className='child_2'>
+
+              <div className='grand_child_1'>
+                <img src={people} alt='people' className='people' />
+              </div>
+
+              <div className='grand_child_2'>
+                <p>Trusted by 500k+ design & product professionals</p>
+              </div>
+
+              <div className='grand_child_3'>
+              </div>
+
+              <div className='grand_child_4'>
+
+                <p></p>
+                <img src='' alt='' className='' />
+                <h5></h5>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </main>
 
 
     </div>
