c218d8d7944cd58fc7f59f9063d678806ef2202b
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-18 14:09:18 +0500

    working header

--- 06-React/Classes/Class-01 (Format setting)/first-app/src/App.js
+++ 06-React/Classes/Class-01 (Format setting)/first-app/src/App.js
@@ -2,22 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+
+      <header className='App-header'>
+
+        <div className='App-parent_1'>
+
+          <img src={logo} className='App-logo' alt='logo' />
+
+        </div>
+
+        <div className='App-parent_2'>
+
+          <a href='#'>Product</a>
+          <a href='#'>Community</a>
+          <a href='#'>Resources</a>
+          <a href='#'>Pricing</a>
+          <a href='#'>For Teams</a>
+
+        </div>
+
+        <div className='App-parent_3'>
+
+          <button className='App-login'>Log In</button>
+          <button className='App-get'>Get started</button>
+
+        </div>
+
+
       </header>
+
+
     </div>
   );
 }
