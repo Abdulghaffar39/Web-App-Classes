c07f506e155bdbdea75b1cb4288ec784089c026a
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-12-05 17:42:20 +0500

    crate vite

---
+++ 06-React/Personal Working/Class-02 (useState)/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Components/Home/index'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+      <Home />
+      <h1>alfjsdfjsadlfasdjflk</h1>
+    </div>
+  )
+}
+
+export default App
