a7e9076393839b995d66073162d08e866ef52ff2
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-12-20 19:27:31 +0500

    useState uses

--- 06-React/Classes/Class-02 (useState,useEffect and routing)/vite_project/src/App.jsx
+++ 06-React/Classes/Class-02 (useState,useEffect and routing)/vite_project/src/App.jsx
@@ -4,7 +4,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [isOn, setIsOn] = useState(0)
+  const [isOn, setIsOn] = useState(false)
+  const [name, setName] = useState("")
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+
+  const addTodo = () => {
+    setTodos([...todos, input]);
+    setInput('');
+  }
 
   return (
     <>
@@ -16,8 +24,32 @@ function App() {
         <button onClick={() => setCount(count + 1)}>Count: {count}</button>
 
         <h1>ON / OFF Button</h1>
-        <button onClick={() => setIsOn(!isOn)}>Toggle</button>
-        <p>Light: {isOn ? "Off" : "On"}</p>
+        <button onClick={() => setIsOn(!isOn)}>Toggle {isOn ? "Off" : "On"}</button>
+
+        {/* <h1>Name</h1>
+
+          <div className='changeName'>
+
+            <p>Salam, {name}</p>
+            <input type="text"
+              value={name}
+              placeholder='Enter your Name'
+              onChange={(e) => setName(e.target.value)}
+            />
+
+          </div> */}
+
+        <div>
+
+          <input value={input} onChange={(e) => setInput(e.target.value)} />
+
+          <button onClick={addTodo}>Add</button>
+
+          <ul>
+            {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+          </ul>
+
+        </div>
 
       </div>
 
